2a01eb0db1f48eafd0ef2d776352b5b13971fccf
mrqrvly <mrqrvly@gmail.com>
2016-06-02 17:05:36 -0500

    imported files from WDI application about me page

---
+++ js/main.js
@@ -0,0 +1,29 @@
+
+
+			// welcome box
+
+
+
+confirm("Welcome to my 'About Me' page!");
+
+
+
+			// mouseover control
+
+
+
+// function popLocate (event) {
+// 	var mouseX = event.clientX;
+// 	var mouseY = event.clientY;
+// }
+
+function show (box) {	
+	// box.style.left=mouseX;
+	// box.style.top=mouseY;
+	box.style.display="block";
+}
+
+function hide (box) {
+	box.style.display="";
+}
+
